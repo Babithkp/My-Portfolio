7f355f773e690cb36cd78c4a48f227c8ed59425d
Babithkp <babithkullachetti44@gmail.com>
2023-08-31 07:42:32 +0530

    my team section have updated

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 AOS.init();
 
+new Glide('.glide').mount();
 
 
-
-// nav bar position
+// fix nav bar position
 let firstPage = document.querySelector(".pageContainer");
 const navBar = document.querySelector("nav");
 const navHeight = navBar.getBoundingClientRect().height;
@@ -34,3 +34,19 @@ new TypeIt(`.${div}`, {
 
 typeit("animate");
 typeit("animates");
+
+var input = document.querySelector('.glide__track')
+
+var glide = new Glide('.glide', {
+  autoplay: input.value,
+  hoverpause: false,
+  perView: 3
+})
+
+input.addEventListener('input', function (event) {
+  glide.update({
+    autoplay: (event.target.value != 0) ? event.target.value : false
+  })
+})
+
+glide.mount()
